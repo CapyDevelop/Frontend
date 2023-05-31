ed5ae6647ebb5e79380b38994d345ae4142fa82d
Victarion Muntz <victarim@at-o6.msk.21-school.ru>
2023-05-31 17:41:00 +0300

    meow

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import Container from "../container/Container";
 import "./Header.css"
+import logo from './../../img/logo.svg'
 function Header() {
     return(
         <div className="header">
             <Container>
                 <header>
                     <div className="logo">
-                    <img src="../img/logo.svg" alt="Capibaras Tribe"></img>  
+                    <img src={logo} alt="Capibaras Tribe"></img>  
                     </div>
                     <div className="namelogo">
                     <p>Capibaras <br/>Tribe</p> 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,27 +1,49 @@
 import Container from "../container/Container";
 import "./Main.css"
+import img from './../../img/capi1.jpg'
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
 function Main() {
+    const [login, setLogin] = useState("")
+
+    useEffect(() => {
+        console.log(login);
+    }, [login])
+    const send = () => {
+        axios("http://195.133.197.62:3000/api/check_peer", {
+            method: "POST",
+            data: {
+                "peer": login
+            }
+        })
+        .then(data => console.log(data.data))
+        .catch(e => console.log(e))
+    }
     return(
         <div className="main">
             <Container>
                 <main>
                     <div className="form">
                         <div className="leftpart">
-                        <p className="logining">Вход</p>
-                        <p className="Hi">Привет!</p>
-                        <p className="text1">Авторизируйся через школьный логин,<br/> чтобы перейти к голосованию</p>
-                        <p className="login">Логин:</p>
-                        <div className="field">
-                            <input type="text" className="fieldlogin" placeholder="Capybara"></input>
-                        </div>
-                        <p className="password">Пароль:</p>
-                        <div className="field">
-                            <input type="password" className="passwordstars" placeholder="* * * * * *"></input>
-                        </div>
-                        <button className="buttonlogIn">Войти</button>
+                            <div className="leftpart-block">
+                                <p className="logining">Вход</p>
+                                <p className="Hi">Привет!</p>
+                                <p className="text1">Авторизируйся через школьный логин,<br/> чтобы перейти к голосованию</p>
+                                <p className="login">Логин:</p>
+                                <div className="field">
+                                    <input type="text" className="fieldlogin" placeholder="Capybara" onChange={e => setLogin(e.target.value)}/>
+                                </div>
+                                <p className="password">Пароль:</p>
+                                <div className="field">
+                                    <input type="password" className="passwordstars" placeholder="* * * * * *"/>
+                                </div>
+                                <button onClick={() => send()} className="buttonlogIn">Войти</button>
+                            </div>
+                            
                         </div>
                         <div className="rightpart">
-                            <img src="../img/capi1.png"></img>
+                            <img src={img}></img>
                         </div>
                     </div>
                 </main>
